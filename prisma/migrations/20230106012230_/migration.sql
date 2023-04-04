87249d44f9bfb91a4b9ddd1f149402abfede50db
VoduVincent <VoduMapping@gmail.com>
2023-04-04 17:17:35 +0200

    restart time

--- prisma/migrations/20230106012230_/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Task" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "done" BOOLEAN NOT NULL,
-    "time" INTEGER NOT NULL
-);
--- src/routes/task-list/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import Navbar from "$lib/components/Navbar.svelte";
-    import TaskList from "$lib/components/task-list/task-list.svelte";
-    import "../../app.css"
-</script>
-<Navbar/>
-
-<TaskList/>
