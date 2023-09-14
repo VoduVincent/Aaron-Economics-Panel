bd27df14a94fea6478e65af54c8123521f2dd257
Vincent Böhm <Vincent.Boehm2004@gmail.com>
2023-09-14 19:23:20 +0200

    Reorganized for better UI

---
+++ src/lib/nav.svelte
@@ -0,0 +1,11 @@
+<nav>
+    <li class="flex-inline bg-blue-300 h-12">
+		<div class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full fixed left-3/4 top-1">
+			<a href="/earth">Earth Dashboard</a>
+		</div>
+
+		<div class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full fixed right-3/4 top-1">
+			<a href="/wuxia">Wuxia Dashboard</a>
+		</div>
+	</li>
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import "../app.css";
+	import Nav from "$lib/nav.svelte";
+</script>
+<html lang="en">
+	<Nav/>
+	<slot>
+		
+	</slot>
+</html>
