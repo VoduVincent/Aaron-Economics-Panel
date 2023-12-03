fbcb7d4dfd31e9db1ed830a66d839bf903083966
Vodu <VoduMapping@gmail.com>
2023-12-03 17:37:19 +0100

    rework rework

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,18 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter()
-  },
-  vite: {
-    server: {
-      // Configure the server to serve files with the .db extension from the lib folder
-      // This sets up a route that will serve .db files from the "lib" folder
-      // You can specify other extensions or routes as needed.
-      // Make sure to adjust the path to your project structure.
-      serve: {
-        '/lib': './lib/*.db',
-      },
-    },
-  },
-  preprocess: vitePreprocess()
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
+
 export default config;
