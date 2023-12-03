5bee7cf19d014f8d49e95ebc192709cd9db004a7
Vodu <VoduMapping@gmail.com>
2023-12-03 19:11:02 +0100

    Collapse of your 404

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import brain from "$lib/images/aaron_brain.png"
+</script>
+
+<h1 role="main">ERROR 404 BRAIN NOT FOUND</h1>
+
+<div>
+    <img src={brain} alt="aaron-brain">
+</div>
