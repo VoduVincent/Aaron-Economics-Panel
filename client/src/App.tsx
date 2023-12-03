3f64877574bbec6944e6102a12659777c85375ea
Vodu <VoduMapping@gmail.com>
2023-12-03 05:43:07 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aaron god Panel
+          Aaron God Panel
         </p>
         <a
           className="App-link"
