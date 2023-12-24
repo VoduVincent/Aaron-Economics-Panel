d78d360a452539437c05baf0134f6226391fb618
Vodu <VoduMapping@gmail.com>
2023-12-25 00:52:59 +0100

    rework 2

--- src/routes/+error.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import brain from "$lib/images/aaron_brain.png"
-</script>
-
-<h1 role="main">ERROR 404 BRAIN NOT FOUND</h1>
-
-<div>
-    <img src={brain} alt="aaron-brain">
-</div>
