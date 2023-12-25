49b54799faea94e6da3dadfec92c6e90687d7894
Vodu <VoduMapping@gmail.com>
2023-12-25 19:13:57 +0100

    direct db acsses

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -2,6 +2,9 @@
   <ul>
     <li><strong><a href="/">Aaron God Panel</a></strong></li>
   </ul>
+  <ul class="container">
+    <li><a href="db.aarongodpanel.com" role="button" color="Pink">Direct God DB</a></li>
+  </ul>
   <ul>
     <li><a href="/earth" role="button">Earth Dashboard</a></li>
     <li><a href="/wuxia" role="button">Wuxia Dashboard</a></li>
