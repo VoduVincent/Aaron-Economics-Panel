ba66f9f43ebe5b89c9608a7c70f03d88e50014a1
Vodu <VoduMapping@gmail.com>
2023-12-25 22:34:31 +0100

    no more csv

--- src/routes/api/month/+server.ts
+++ src/routes/api/month/+server.ts
@@ -1,11 +1,10 @@
 import { prisma } from "$lib/handler/db/prisma";
-import type { RequestHandler } from "@sveltejs/kit";
+import { json, type RequestHandler } from "@sveltejs/kit";
 
 import Papa from "papaparse";
 
 export async function GET({url}) {
     const month_param = Number(url.searchParams.get("month") ?? 1)
-    const csv_param = Number(url.searchParams.get("csv") ?? 0)
 
     if( await prisma.month.findUnique({
         where:{
@@ -13,33 +12,25 @@ export async function GET({url}) {
         }
     }))
     {
-        const month = await prisma.month.findUnique({where:{id:month_param}})
-        const planet = await prisma.month.findUnique({where:{id:month_param}}).planet()
-        const surface = await prisma.month.findUnique({where:{id:month_param}}).surface()
-        const nation = await prisma.month.findUnique({where:{id:month_param}}).nation()
-        const resource = await prisma.month.findUnique({where:{id:month_param}}).resource()
-        const resource_type = await prisma.month.findUnique({where:{id:month_param}}).resource_type()
-        const species = await prisma.month.findUnique({where:{id:month_param}}).species()
-        const city = await prisma.month.findUnique({where:{id:month_param}}).city()
+    const month = await prisma.month.findUnique({where:{id:month_param}})
+    const planet = await prisma.month.findUnique({where:{id:month_param}}).planet()
+    const surface = await prisma.month.findUnique({where:{id:month_param}}).surface()
+    const nation = await prisma.month.findUnique({where:{id:month_param}}).nation()
+    const resource = await prisma.month.findUnique({where:{id:month_param}}).resource()
+    const resource_type = await prisma.month.findUnique({where:{id:month_param}}).resource_type()
+    const species = await prisma.month.findUnique({where:{id:month_param}}).species()
+    const city = await prisma.month.findUnique({where:{id:month_param}}).city()
 
-    if (csv_param){
-            const data = [
-                {month},
-                {planet},
-                {surface},
-                {nation},
-                {resource},
-                {resource_type},
-                {species},
-                {city}
-            ]
-            return new Response(Papa.unparse(data))
-        }
+    const data = {
+        month:month,
+        planet:planet,
+        surface:surface,
+        nation:nation,
+        resource:resource,
+        resource_type:resource_type,
+        species:species,
+        city:city
     }
-    else{
-        return new Response("How about you give me a month that actually exists dumbass")
+    return json(data)
     }
-
-    return new Response("This code and seethe aaron")
-
 }
